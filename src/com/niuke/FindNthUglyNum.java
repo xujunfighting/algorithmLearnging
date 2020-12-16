bc84bf2bdf8e19da6eb6b0178545ed0a34179d07
徐骏 <02005609@yto.net.cn>
2020-12-16 11:34:28 +0800

    ugly method enchance

--- src/com/niuke/FindNthUglyNum.java
+++ src/com/niuke/FindNthUglyNum.java
@@ -1,11 +1,13 @@
 package com.niuke;
 
+import java.util.HashMap;
+
 public class FindNthUglyNum {
     public static void main(String[] args) {
-        int result = getResult(7);
+        int result = getResult2(7);
         System.out.println(result);
     }
-    public static int getResult(int num){
+    public static int getResult1(int num){
         int[] indexs=new int[num];
         indexs[0]=1;
         for(int i=1;i<num;i++){
@@ -25,6 +27,25 @@ public class FindNthUglyNum {
                    indexs[i]=min;
                }
            }
+        }
+        return indexs[num-1];
+    }
+    public static int getResult2(int num){
+        int[] indexs=new int[num];
+        indexs[0]=1;
+        int start=1;
+        HashMap<Integer,Integer> m=new HashMap<Integer,Integer>();
+        m.put(start,start);
+        for(int i=1;i<num;i++){
+            boolean isGet=false;
+            while(!isGet){
+                start++;
+                if((start%2==0&&m.containsKey(start/2))||(start%3==0&&m.containsKey(start/3))||(start%5==0&&m.containsKey(start/5))){
+                    indexs[i]=start;
+                    isGet=true;
+                    m.put(start,start);
+                }
+            }
         }
         return indexs[num-1];
     }
