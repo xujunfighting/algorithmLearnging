d7cfa2d3dd40c7b1cc347c1f17d397e968e26ef3
徐骏 <02005609@yto.net.cn>
2021-07-23 10:13:18 +0800

    提交判断是否是合法的二叉查找树题解

--- src/com/niuke/tree/IsValidBinarySearchTree.java
+++ src/com/niuke/tree/IsValidBinarySearchTree.java
@@ -1,6 +1,7 @@
 package com.niuke.tree;
 
 import com.zto.algorithm.TreeNode;
+import sun.reflect.generics.tree.Tree;
 
 /**
  * @version v1.0.0
@@ -12,10 +13,27 @@ import com.zto.algorithm.TreeNode;
 public class IsValidBinarySearchTree {
     /**
      * 用于记录以当前节点作为根节点的左子树的最大值 和右子树的最小值
+     * 需要满足根节点大于左子树的最大值 且小于右子树的最小值
      */
     static int[] res=new int[3];
+    static boolean result=true;
+    //[5,4,6,null,null,3,7]
+    //[32,26,47,19,null,null,56,null,27].
     public static void main(String[] args) {
+          TreeNode n32=new TreeNode(32);
+          TreeNode n26=new TreeNode(26);
+          TreeNode n47=new TreeNode(47);
+          TreeNode n19=new TreeNode(19);
+          TreeNode n56=new TreeNode(56);
+          TreeNode n27=new TreeNode(27);
+          n32.left=n26;
+          n26.left=n19;
+          n19.right=n27;
+          n32.right=n47;
+          n47.right=n56;
 
+          getResult(n32);
+        System.out.println(result);
     }
     public static boolean getResult(TreeNode node){
         if(node==null){
@@ -29,15 +47,42 @@ public class IsValidBinarySearchTree {
         }
         int val=node.val;
         boolean left=getResult(node.left);
-        int leftL=res[0];
-        int leftR=res[1];
+        int leftMIn=res[0];
+        int leftMax=res[2];
         boolean right=getResult(node.right);
-        int rightL=res[0];
-        int rightR=res[1];
+        int rightMin=res[0];
+        int rightMax=res[2];
         if(left&&right){
-            if(le)
+            if((node.left!=null&&val>leftMax)&&(node.right!=null&&val<rightMin)){
+                /**
+                 * 左右子树都不为空 记录左子树的最小值 和右子树的最大值
+                 */
+                res[0]=leftMIn;
+                res[1]=val;
+                res[2]=rightMax;
+                result=true;
+            }else if(node.right==null&&node.left!=null&&val>leftMax){
+                /**
+                 * 右子树为空
+                 */
+                res[0]=leftMIn;
+                res[1]=val;
+                res[2]=val;
+                result=true;
+            }else if(node.left==null&&node.right!=null&&val<rightMin){
+                /**
+                 * 左子树为空
+                 */
+                res[0]=val;
+                res[1]=val;
+                res[2]=rightMax;
+                result=true;
+            }else{
+                result=false;
+            }
         }else{
-            return false;
+            result=false;
         }
+        return result;
     }
 }
